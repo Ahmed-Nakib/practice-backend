9125a5c5d14d2740b843c566ecf8b6cc0292b118
Ahmed-Nakib <office.nakib@gmail.com>
2026-05-15 23:48:47 +0600

    node api raw

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,13 @@
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-import app from "./app"
+import { createServer, IncomingMessage, Server, ServerResponse } from "http";
+import config from "./config";
+import { routeHandler } from "./routes/route";
 
-dotenv.config();
+const server: Server = createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    routeHandler(req, res);
+  },
+);
 
-const port = process.env.PORT || "5000";
-
-const uri = process.env.MONGODB_URI as string;
-
-
-const bootstrap = async () => {
-
-  await mongoose.connect(uri)
-  console.log("Mongodb Connected");
-  
-    
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`)
-    })
-}
-
-
-bootstrap()
+server.listen(config.port, () => {
+  console.log(`The server is running on the port ${config.port}`);
+});
