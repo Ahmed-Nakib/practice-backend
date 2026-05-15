8b999fe6dcabf4e236ce05289d76637defd3de55
Ahmed-Nakib <office.nakib@gmail.com>
2026-05-15 23:58:43 +0600

    node-express-postgresql

--- oxfmt.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "oxfmt";
-
-export default defineConfig({
-  ignorePatterns: ["dist/**", "node_modules/**", "public/**"],
-  bracketSameLine: true,
-  jsdoc: true,
-  sortImports: true,
-});
